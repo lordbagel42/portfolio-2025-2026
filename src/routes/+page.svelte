3f5ff253f5d3b99639189aace18764985f838f0d
Raygen Rupe <raygenrrupe@gmail.com>
2026-03-18 07:06:43 -0600

    im a button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import { Button } from "$lib/components/ui/button";
+	import { Button } from '$lib/components/ui/button';
 </script>
 
-<div class="flex flex-col items-center justify-center min-h-screen gap-4">
-	<h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://svelte.dev/docs/kit" class="text-primary hover:underline">svelte.dev/docs/kit</a> to read the documentation</p>
-	<Button>Shadcn Button</Button>
+<div class="flex min-h-screen flex-col items-center justify-center gap-4">
+	<h1 class="text-4xl font-bold">portfolio</h1>
+	<Button>im a button</Button>
 </div>
